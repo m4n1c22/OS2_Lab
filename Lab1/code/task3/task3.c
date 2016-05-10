e64a1252e27443836282595291a0797d3bfc79a6
gizem <gizemgulesir@gmail.com>
2016-05-10 13:47:56 +0200

    sigpipe error is generated.

--- Lab1/code/task3/task3.c
+++ Lab1/code/task3/task3.c
@@ -6,51 +6,68 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
 
-int backup_terminated (int fork_ret_val, int fd[2]) {
-	int ret_val_write,ret_val_usleep;
+void sighandler(int signum)
+{
+	// TODO here change server process into backup process clear state
+	/* Change the server process into a backup process. A simple and clean way to achieve this
+	is to replace the current server process image by a fresh instance of your program. This has
+	the same effects as if you kill and restart your whole program. See man 3 exec for details
+	on how to achieve this. Note that the original command line options have to survive this
+	process image replacement.*/
+	if (signum == SIGPIPE) {
+		printf("Caught signal %d pipe read end closed\n", signum);
+		printf("Parent (backup) process failed\n");
+	} else if (signum == SIGINT) {
+		printf("Caught signal %d, interrupt\n", signum);
+	}	
+	
+	exit(EXIT_SUCCESS);
+}
+
+int backup_terminated (int fork_ret_val, int fd[2], int read_count) {
+	int ret_val_write,ret_val_usleep, ret_val_close;
 	char buffer;
 	char poll_message[] = "Hallo I am child (server) process, this is a polling message\n";
-
+	signal(SIGINT, sighandler);
+	signal(SIGPIPE, sighandler);
 	// child (server) process
 	if (fork_ret_val == 0) {
 	        do {
 			// reduce the polling frequency
 			ret_val_usleep = usleep(500000);
 			// failure in usleep call
-			if(ret_val_usleep<0) {
+			if(ret_val_usleep < 0) {
 				fprintf(stderr, "Error: usleep function failed.\n");
 				// process returns and terminates with error
 				return EXIT_FAILURE;
 			}
 			ret_val_write = write(fd[1], poll_message, strlen(poll_message));
-			// TODO Error creation for testing
-			/* child gets error, this usually only works if the signal SIGPIPE is ignored, see man 2 signal */
 			if (ret_val_write < 0) {
 				fprintf(stderr, "Error: write function failed.\n");
-				printf("Parent (backup) process failed\n");
-
-				// TODO here change server process into backup process clear state
-				/* Change the server process into a backup process. A simple and clean way to achieve this
-				is to replace the current server process image by a fresh instance of your program. This has
-				the same effects as if you kill and restart your whole program. See man 3 exec for details
-				on how to achieve this. Note that the original command line options have to survive this
-				process image replacement.*/
-
 				// process returns and terminates with error
 				return EXIT_FAILURE;
 			}
 	        } while (ret_val_write > 0);
-
 	// parent (backup) process
 	} else if ( fork_ret_val > 0) {
 		while (read(fd[0], &buffer, 1) > 0) {
-                    ret_val_write = write(STDOUT_FILENO, &buffer, 1);
-		    if(ret_val_write < 0) {
-			fprintf(stderr, "Error: write function failed.\n");
-			// process returns and terminates with error
-			return EXIT_FAILURE;
-		    }
+			// After 150th read close the read end of parent to generate SIGPIPE error at child
+			if (read_count == 0) {
+				ret_val_close = close(fd[0]);
+				if(ret_val_close < 0) {
+	       				fprintf(stderr, "Error: close function failed.\n");
+					return EXIT_FAILURE;
+	 			}
+			}
+                    	ret_val_write = write(STDOUT_FILENO, &buffer, 1);
+		    	if(ret_val_write < 0) {
+				fprintf(stderr, "Error: write function failed.\n");
+				// process returns and terminates with error
+				return EXIT_FAILURE;
+		   	}
+			read_count--;
 	        }
 
                 ret_val_write = write(STDOUT_FILENO, "\n", 1);
@@ -68,10 +85,10 @@ int backup_terminated (int fork_ret_val, int fd[2]) {
 	return EXIT_SUCCESS;
 }
 
-int test_task3() {
+int test_task3(int read_count) {
 	int pipefd[2], ret_val_pipe, ret_val_close;
         pid_t ret_val_fork;
-        
+        signal(SIGPIPE, SIG_IGN);
         ret_val_pipe = pipe(pipefd);
 	if (ret_val_pipe < 0) {
 		fprintf(stderr, "Error: pipe function failed.\n");
@@ -95,7 +112,7 @@ int test_task3() {
 	 	}
 		printf("I am the child process. My PID is %d\n", getpid());
 		// child (server) process will continuously poll the parent (backup) process
-		backup_terminated (ret_val_fork, pipefd);
+		backup_terminated (ret_val_fork, pipefd, read_count);
 		// parent (backup) process will see EOF
                 ret_val_close = close(pipefd[1]);
 		if(ret_val_close < 0) {
@@ -113,7 +130,7 @@ int test_task3() {
 	 	printf("I am the parent process. My PID is %d\n", getpid());
 		printf("Creation of child successful. Child's PID is %d\n", ret_val_fork);
 		// parent (backup) process will continuously receive poll message from child (server) process
-		backup_terminated (ret_val_fork, pipefd);
+		backup_terminated (ret_val_fork, pipefd, read_count);
                 ret_val_close = close(pipefd[0]);
 		if(ret_val_close < 0) {
 	       		fprintf(stderr, "Error: close function failed.\n");
@@ -128,7 +145,8 @@ int test_task3() {
 
 int main(int argc, char *argv[]) {
 	int ret_val_test_task3;
-	ret_val_test_task3 = test_task3();
+	// Generate error after 150th read of parent process
+	ret_val_test_task3 = test_task3(150);
 	if(ret_val_test_task3 != EXIT_SUCCESS) {
 		fprintf(stderr, "Error:Testing Task3 Failed.\n");
 		exit(EXIT_FAILURE);
