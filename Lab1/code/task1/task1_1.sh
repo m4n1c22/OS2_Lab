cf1086624388ab02005853eea928002b7bbf1769
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2016-05-11 17:38:37 +0200

    Error count and success count added.

--- Lab1/code/task1/task1_1.sh
+++ Lab1/code/task1/task1_1.sh
@@ -2,7 +2,8 @@
 
 #configurable value for the test_runs counter.
 test_runs=5
-
+error_count=0
+success_count=0
 count=0
 
 #check if the log file exists or not.
@@ -16,11 +17,13 @@ do
 	make exec >> overall.log
 	exit_code=$?
 	echo Exit code : $exit_code
-
 	error=$(grep -q "Error" overall.log)
 	if [ "$exit_code" != 0 ]
 	then
 		echo "Error found"
+		error_count=`expr $error_count + 1`
 	fi
 	count=`expr $count + 1`
 done
+success_count=`expr $test_runs - $error_count`
+echo Program executed $test_runs times with $error_count erroneous executions and $success_count successful executions
--- Lab1/code/task1/task1_3.sh
+++ Lab1/code/task1/task1_3.sh
@@ -2,7 +2,8 @@
 
 #configurable value for the test_runs counter.
 test_runs=10
-
+error_count=0
+success_count=0
 count=0
 
 #check if the log file exists or not.
@@ -19,6 +20,9 @@ do
 	if [ "$exit_code" != 0 ]
 	then
 		echo "Error found"
+		error_count=`expr $error_count + 1`
 	fi
 	count=`expr $count + 1`
 done
+success_count=`expr $test_runs - $error_count`
+echo Program executed $test_runs times with $error_count erroneous executions and $success_count successful executions
